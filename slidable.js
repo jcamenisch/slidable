4f9d4752237f1ddd06e9c9f0b040582ebed3c5ae
Jonathan Camenisch <jonathan@camenisch.net>
2012-04-03 17:23:52 -0500

    Allow for margins between items; remove 'hiddenDimension' function

--- slidable.js
+++ slidable.js
@@ -1,39 +1,4 @@
 (function($, undefined){
-  $.fn.hiddenDimension = function(){
-    if (arguments.length && typeof arguments[0] == 'string') {
-      var dimension = arguments[0]
-
-      if (this.is(':visible')) return this[dimension]();
-
-      var visible_container = this.closest(':visible');
-
-      if (!visible_container.is('body')) {
-        var
-          container_clone = $('<div />')
-            .append(visible_container.children().clone())
-            .css({
-              position: 'absolute',
-              left:'-32000px',
-              top: '-32000px',
-              width: visible_container.width(),
-              height: visible_container.height()
-            })
-            .appendTo('body'),
-          element_index = $('*',visible_container).index(this),
-          element_clone = $('*',container_clone).slice(element_index);
-        
-        element_clone.parentsUntil(':visible').show();
-        
-        var result = element_clone[dimension]();
-        container_clone.remove();
-        return result;
-      } else {
-        //TO-DO: support elements whose nearest visible ancestor is <body>
-        return undefined
-      }
-    }
-    return undefined //nothing implemented for this yet
-  }
 
   var spriteButton = function(kind) {
     return $('<button class="'+kind+'" />');
@@ -64,10 +29,10 @@
         var
           vertical = ('columns' in options),
           horizontal = !vertical,
-          itemWidth = items.hiddenDimension('outerWidth'),
-          itemHeight = items.hiddenDimension('outerHeight'),
-          wrapperWidth = wrapper.hiddenDimension('width'),
-          wrapperHeight = wrapper.hiddenDimension('height'),
+          itemWidth = items.outerWidth(true),
+          itemHeight = items.outerHeight(true),
+          wrapperWidth = wrapper.width(),
+          wrapperHeight = wrapper.height(),
           itemsWide = Math.round(wrapperWidth/itemWidth),
           listWidth = itemsWide * itemWidth,
           itemsHigh = options.rows || Math.round(wrapperHeight/itemHeight),
@@ -95,7 +60,7 @@
         ;
 
         options = $.extend({
-          duration: 300 + Math.round(list.slide_increment * items.outerWidth()),
+          duration: 300 + Math.round(list.slide_increment * items.outerWidth(true)),
           easing: 'swing'
         }, options);
         list.options = options;
@@ -122,7 +87,7 @@
               list.slide_position = position;
               if (typeof list.options.beforeScroll === 'function') list.options.beforeScroll.call(list, list.slide_position);
               list.animate(
-                {left: -list.children().outerWidth() * list.slide_position + 'px'},
+                {left: -list.children().outerWidth(true) * list.slide_position + 'px'},
                 list.options.duration, list.options.easing,
                 function(){
                   if (typeof list.options.afterScroll === 'function') list.options.afterScroll.call(list, list.slide_position);
